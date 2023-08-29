46e364b2564dd8be298dec0a3a0b6b6e771eef15
sahlminkok <sahlminkok7@gmail.com>
2023-08-29 23:45:33 +0100

    Add author_id to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_223438) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_224203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,16 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_223438) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.integer "author_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,5 +44,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_223438) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "posts", "users"
 end
