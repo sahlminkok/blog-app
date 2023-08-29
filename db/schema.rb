dad815e90084241175c1e1e80c5a312aa9c818ac
sahlminkok <sahlminkok7@gmail.com>
2023-08-29 23:33:30 +0100

    Add author_id to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_222121) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_223059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_222121) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -28,9 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_222121) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
