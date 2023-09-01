b75922a5004f41aac1771e78918461884f808228
sahlminkok <sahlminkok7@gmail.com>
2023-09-01 11:16:16 +0100

    Add a unit test for five_most_recent_comments method in post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject do
-    Post.new(title: 'Ruby on Rails',
-             text: 'Learning RoR is a bit hard, but with great mindset you can overcome the difficulties and understan',
-             author: User.create(name: 'Sahalu'))
-  end
+  subject { Post.new(title: 'RoR', author: User.create(name: 'Sahalu'), comments_counter: 0, likes_counter: 0) }
   before { subject.save }
 
   it 'title should be present' do
@@ -38,5 +34,22 @@ RSpec.describe Post, type: :model do
   it 'likes counter should be greater than or equal to 0' do
     subject.likes_counter = -1
     expect(subject).to_not be_valid
+  end
+
+  describe '#five_most_recent_comments' do
+    it 'should return the five most recent comments of a given post' do
+      user = User.create(name: 'Sahalu')
+
+      Comment.create(text: 'comment1', author: user, post: subject, created_at: 5.days.ago)
+      comment2 = Comment.create(text: 'comment2', author: user, post: subject, created_at: 4.days.ago)
+      comment3 = Comment.create(text: 'comment3', author: user, post: subject, created_at: 3.days.ago)
+      comment4 = Comment.create(text: 'comment4', author: user, post: subject, created_at: 2.days.ago)
+      comment5 = Comment.create(text: 'comment5', author: user, post: subject, created_at: 1.day.ago)
+      comment6 = Comment.create(text: 'comment6', author: user, post: subject, created_at: Time.now)
+
+      expected_order = [comment6, comment5, comment4, comment3, comment2]
+
+      expect(subject.five_most_recent_comments).to eq(expected_order)
+    end
   end
 end
