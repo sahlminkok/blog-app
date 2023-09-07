ff600d0c41947cfd3f4f929916395a7a06fc61ef
sahlminkok <sahlminkok7@gmail.com>
2023-09-07 15:52:10 +0100

    Add new method and create method in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully!'
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
