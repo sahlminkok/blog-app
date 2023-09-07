3b5e6f648f8c044077420bc845f5cc19382438a8
sahlminkok <sahlminkok7@gmail.com>
2023-09-07 22:46:26 +0100

    Add routes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :show, :create]
+    resources :posts, only: [:index, :new, :show, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end
