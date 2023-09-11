0be2880ebbd2aaa1cb1a7cf7b12bfea7e820fb14
sahlminkok <sahlminkok7@gmail.com>
2023-09-11 22:51:44 +0100

    Delete commented code

--- spec/system/users/index_spec.rb
+++ spec/system/users/index_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Index Page' do
-  # before {  }
   it 'should display user information' do
     user = User.create(name: 'Sahalu', photo: 'https://placehold.co/200x133', posts_counter: 0)
     user1 = User.create(name: 'Kabir', photo: 'https://placehold.co/200x133', posts_counter: 0)
