4ec85597d05a9586c98987dae9766fde892706a3
sahlminkok <sahlminkok7@gmail.com>
2023-09-11 22:49:33 +0100

    Add test for redirection to user's show page when clicked on the user

--- spec/system/users/index_spec.rb
+++ spec/system/users/index_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Index Page' do
+  # before {  }
   it 'should display user information' do
     user = User.create(name: 'Sahalu', photo: 'https://placehold.co/200x133', posts_counter: 0)
     user1 = User.create(name: 'Kabir', photo: 'https://placehold.co/200x133', posts_counter: 0)
@@ -14,5 +15,14 @@ RSpec.describe 'Users Index Page' do
       expect(page).to have_css("img[src*='#{u.photo}']")
       expect(page).to have_content("Number of posts: #{u.posts_counter}")
     end
+  end
+
+  it "should redirect to user's show page when clicked on a user" do
+    user = User.create(name: 'Sahalu', photo: 'https://placehold.co/200x133', posts_counter: 0)
+
+    visit users_path
+    
+    find("a[href='/users/#{user.id}']", visible: :all).click
+    expect(page).to have_current_path(user_path(user))
   end
 end
