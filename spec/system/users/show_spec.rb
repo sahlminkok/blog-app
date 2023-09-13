4869d4a860300278eb88aad748f4e4023cf6888e
Luis Zubia <soyzubia@gmail.com>
2023-09-13 00:13:50 -0700

    Add 'test' for 'users#show' to check: displays the username

---
+++ spec/system/users/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'User page [users#show]', type: :system do
+  before(:all) do
+    john_bio = ['This is the user bio.', 'it has', 'several', 'rows']
+    @user = User.create(name: 'John Doe', photo: 'https://plchldr.co/i/75?bg=252f3f&fc=ffffff&text=john',
+                        bio: john_bio.join(' '), posts_counter: 0)
+    %w[post1 post2 post3 post4 post5].each { |p| Post.create(author: @user, title: p, text: "#{p} message") }
+  end
+
+  describe '* shows user\'s info like:' do
+    it '- the user name' do
+      visit user_path(@user)
+      expect(page).to have_content(@user.name)
+    end
+  end
+end
