6cad8b7a2fb2b28322dd8c0df99033a44fcddf9b
Luis Zubia <soyzubia@gmail.com>
2023-09-13 02:54:56 -0700

    Add code for testing environment on 'posts#show' page

---
+++ spec/system/posts/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Post page [posts#show]', type: :system do
+  before(:all) do
+    qty = %w[1 2 3 4 5 6 7 8]
+    john_bio = ['This is the user bio.', 'it has', 'several', 'rows']
+    @user = User.create(name: 'John Doe', photo: 'https://plchldr.co/i/75?bg=252f3f&fc=ffffff&text=john',
+                        bio: john_bio.join(' '), posts_counter: 0)
+    @users = []
+    qty.each do |u|
+      @users << User.create(name: "user #{u}", photo: "https://plchldr.co/i/75?bg=252f3f&fc=ffffff&text=user-#{u}",
+      bio: "This is the user #{u} bio.", posts_counter: 0)
+    end
+    @posts = []
+    qty.each do |p|
+      @posts << Post.create(author: @user, title: "Title #{p}", text: "Post #{p} message", comments_counter: 0, likes_counter: 0)
+    end
+    @posts.each do |p|
+      (1..'123456789'.chars.sample(7).sample.to_i).each do |c|
+        u = @users.sample(6).sample
+        Comment.create(author: u, text: "This is comment #{c}, by #{u.name}", post: p)
+      end
+    @posts.each do |p|
+      (1..'123456'.chars.sample(5).sample.to_i).each do |_c|
+        u = @users.sample(5).sample
+        Comment.create(author: u, post: p)
+      end
+    end
+  end
+end
