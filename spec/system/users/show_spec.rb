4a15009e96d223301893b364d01bc2fc62981b3d
sahlminkok <sahlminkok7@gmail.com>
2023-09-14 10:39:25 +0100

    Fix linter errors

--- spec/system/users/show_spec.rb
+++ spec/system/users/show_spec.rb
@@ -88,7 +88,7 @@ RSpec.describe 'User page [users#show]', type: :system do
         expect(page).to have_current_path(user_posts_path(@user))
       end
     end
-    
+
     context '- a \'back\' button' do
       it '> is shown at the bottom of the page' do
         expect(page).to have_link('Back')
