9b972ca69d34a1cfb9cce695c6eef4c529312e65
sahlminkok <sahlminkok7@gmail.com>
2023-09-16 09:23:32 +0100

    Add destroy in posts resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :show, :create] do
+    resources :posts, only: [:index, :new, :show, :create, :destroy] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
