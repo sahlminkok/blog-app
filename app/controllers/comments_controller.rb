20f999ecc876e5774086b7e5c3a20f91f3b4b14a
Luis Zubia <soyzubia@gmail.com>
2023-09-15 23:45:57 -0700

    Replace the 'fixed' 'current_user' by the one retrieved with 'params[:user_id]' when we add a new comment && Create 'destroy' method for 'comments#destroy' action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,32 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
-    @user = current_user
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
   end
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @comment.author = current_user
+    @comment.author = User.find(params[:user_id])
 
     if @comment.save
       redirect_to user_post_path(current_user, @post), notice: 'Comment was successfully created.'
     else
       render :new
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    authorize! :destroy, @comment
+      
+    if @comment.destroy
+      redirect_to user_post_path(@comment.author, @comment.post), notice: 'Comment was successfully deleted.'
+    else
+      redirect_to user_post_path(@comment.author, @comment.post), alert: 'Error: Comment could not be deleted'
+    end    
   end
 
   private
