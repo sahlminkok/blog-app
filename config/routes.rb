2742d45a0e9cd3e4ef4166f947f245db604eca60
Luis Zubia <soyzubia@gmail.com>
2023-09-15 23:39:47 -0700

    Add route for 'destroy' action for a given 'comment'

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :show, :create, :destroy] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
