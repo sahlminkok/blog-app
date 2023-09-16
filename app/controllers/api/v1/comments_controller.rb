fd4c6d5ec8fc168d33a6a1fd175baa986fef172a
Luis Zubia <soyzubia@gmail.com>
2023-09-16 04:00:04 -0700

    Add 'create' action method for 'api/comments' Controller in order to be able for create comments using the 'api'

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,4 +4,16 @@ class Api::V1::CommentsController < ApplicationController
     @comments = @post.comments
     render json: @comments
   end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.new(comment_params)
+    comment.user = current_user
+
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
