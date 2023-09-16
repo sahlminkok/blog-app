a9bfc62bb2564804d4baa4d93cd928b68b031425
sahlminkok <sahlminkok7@gmail.com>
2023-09-16 19:45:13 +0100

    Add API v1 users and posts index routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,12 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index]
+      end
+    end
+  end
 end
