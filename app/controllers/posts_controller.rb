f24dfe53c100e4f5056ead9f0793549cf964ffb1
sahlminkok <sahlminkok7@gmail.com>
2023-09-16 09:24:47 +0100

    Add destroy action to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,14 +15,25 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
-    @post.comments_counter = 0
-    @post.likes_counter = 0
+    authorize! :create, @post
 
     if @post.save
       redirect_to user_posts_path(current_user), notice: 'Post created successfully!'
     else
       flash.now[:error] = 'Error: Post could not be saved'
       render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = @user.posts.includes(:comments).find_by(id: params[:id])
+    authorize! :destroy, @post
+
+    if @post.destroy
+      redirect_to user_posts_path(@user), notice: 'Post deleted successfully!'
+    else
+      redirect_to user_posts_path(@user), alert: 'Error: Post could not be deleted'
     end
   end
 
